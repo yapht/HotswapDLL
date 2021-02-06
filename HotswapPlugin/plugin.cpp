9a4b881f68fecc0704abe0368fa290a5f5ecd768
yapht <discordaccj@gmail.com>
2021-02-06 16:33:56 -0700

    fix

--- HotswapPlugin/plugin.cpp
+++ HotswapPlugin/plugin.cpp
@@ -1,5 +1,3 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-
 #include <cassert>
 #include <Windows.h>
 #include <iostream>
@@ -30,16 +28,11 @@ int on_close()
 CR_EXPORT int cr_main(struct cr_plugin* ctx, enum cr_op operation) {
     assert(ctx);
 
-
     switch (operation) {
     case CR_LOAD:       return on_load(ctx->userdata); // loading back from a reload
     case CR_UNLOAD:     return on_unload(); // preparing to a new reload
     case CR_CLOSE:      return on_close(); // the plugin will close and not reload anymore
     }
-
-    printf("Cade REALLY Sux.\n");
-
-    *(int*)5 = 3;
 
     return 0;
 }
